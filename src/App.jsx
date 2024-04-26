48c390904334d9c37986968da55e36f86de0adb2
DarkDrinker <Anders.wade.jensen@hotmail.com>
2024-04-26 16:20:59 +0200

    CP

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import upper, {text1,text2, text3} from "./file1";
+import obj from "./file2";
+import { males, females } from "./file2";
+
+
+function App() {
+  const {firstName, email} = obj;
+  let text1test = text1;
+  let text2test = text2;
+  let text3test = text3;
+  const arr = [...males, ...females];
+  const arr2 = [...arr]
+  arr2.push("Helle", "Tina");
+  return (
+    <>
+    <h2>ex 1</h2>
+    <p>{upper(text1test)}</p>
+    <p>{upper(text2test)}</p>
+    <p>{upper(text3test)}</p>
+
+    <h2>ex 2</h2>
+    <p>{firstName},{email}</p>
+    <p>{arr}</p>
+    <p>{arr2}</p>
+
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
